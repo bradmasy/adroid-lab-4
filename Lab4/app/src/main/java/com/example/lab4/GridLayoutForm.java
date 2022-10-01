d3d3bffde551fd162ceb1c95fa48d37da291969e
Brad <bmasciotra@my.bcit.ca>
2022-10-01 15:29:53 -0700

    updated layouts

---
+++ Lab4/app/src/main/java/com/example/lab4/GridLayoutForm.java
@@ -0,0 +1,7 @@
+package com.example.lab4;
+
+import android.app.Activity;
+
+public class GridLayoutForm extends Activity {
+    
+}
---
+++ Lab4/app/src/main/java/com/example/lab4/LinearLayoutForm.java
@@ -0,0 +1,15 @@
+package com.example.lab4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class LinearLayoutForm extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
